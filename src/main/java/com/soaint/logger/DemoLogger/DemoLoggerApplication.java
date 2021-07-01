d4955fa43e3def71faae13dee7400ed8ac1567c7
Luis Patino <luispatinoh@gmail.com>
2021-07-01 17:32:52 -0500

    initial commit

---
+++ src/main/java/com/soaint/logger/DemoLogger/DemoLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.soaint.logger.DemoLogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soaint/logger/DemoLogger/DemoLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soaint.logger.DemoLogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
